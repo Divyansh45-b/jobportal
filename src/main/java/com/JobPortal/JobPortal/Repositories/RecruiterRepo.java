472aed1bdf41ce9ca425beac8b4f119247222ec2
Divyansh <baseradevo123@gmail.com>
2026-09-24 20:31:19 +0530

    Remove duplicate old project structure

--- src/main/java/com/JobPortal/JobPortal/Repositories/RecruiterRepo.java
+++
@@ -1,9 +0,0 @@
-package com.JobPortal.JobPortal.Repositories;
-
-import com.JobPortal.JobPortal.Entities.Recruiter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecruiterRepo extends JpaRepository<Recruiter,Integer> {
-}
